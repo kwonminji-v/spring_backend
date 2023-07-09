65a8c73fe1106dc6c85497ea5f8f5d8a08bfd339
minji6313 <minji6313@yahoo.com>
2023-07-09 23:21:13 +0900

    "싱글톤 진행중"

--- core/src/test/java/Spring/core/singleton/SingletonService.java
+++ core/src/test/java/Spring/core/singleton/SingletonService.java
@@ -5,7 +5,18 @@ public class SingletonService {
     //자기 자신을 내부에 static private으로 선언하고 가지고 있게 됨
     private static final SingletonService instance = new SingletonService();
 
+    //JVM이 실행될 때, 바로 싱글톤 서비스의 스태틱 영역에 자기 자신의 객체를 생성하여 인스턴스를 담음
+    public static SingletonService getInstance() {
+        return instance;
+    }
 
+
+    private SingletonService() {
+    }
+
+    public void logic() {
+        System.out.println("싱글톤 객체 로직 호출");
+    }
 
 
 }
