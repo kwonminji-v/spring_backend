609dc0a0e5c0283b4b9973d3753d25054b8f152a
minji6313 <minji6313@yahoo.com>
2023-07-11 06:27:16 +0900

    "싱글톤끝"

--- core/src/test/java/Spring/core/singleton/ConfigurationSingletonTest.java
+++ core/src/test/java/Spring/core/singleton/ConfigurationSingletonTest.java
@@ -7,6 +7,7 @@ import Spring.core.order.OrderService;
 import Spring.core.order.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -34,4 +35,14 @@ public class ConfigurationSingletonTest {
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
 
     }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        //getBean으로 AppConfig스프링 빈으로 등록된 AppConfig의 클래스 타입을 조회
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean.getClass() = " + bean.getClass());
+    }
+
 }
