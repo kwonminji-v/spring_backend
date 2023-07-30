b297c106fac287f3497dea36ef73a23e12c586e1
minji6313 <minji6313@yahoo.com>
2023-07-30 17:26:20 +0900

    "JPA-1 회원가입 api"

--- jpashop/src/main/java/com/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/com/jpabook/jpashop/api/MemberApiController.java
@@ -8,7 +8,6 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -35,13 +34,19 @@ public class MemberApiController {
     }
 
 
-    @PostMapping("api/v2/members")
+    @PostMapping("/api/v2/members")
     public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
 
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
     }
 
     @Data
     static class CreateMemberRequest {
+        private String name;
 
     }
 
