089726736f9357a79de41b9224d52d52eb231c84
minji6313 <minji6313@yahoo.com>
2023-07-31 17:13:12 +0900

    "#2.SpringMVC HttpHeader-설정법"

--- servlet/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ servlet/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -2,37 +2,33 @@ package hello.servlet.basic.request;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.*;
 
 import java.io.IOException;
-import java.util.Enumeration;
 
+//http://localhost:8080/request-header?username=hello
 @WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
 public class RequestHeaderServlet extends HttpServlet {
-
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    protected void service(HttpServletRequest request, HttpServletResponse
+            response)
+            throws ServletException, IOException {
         printStartLine(request);
         printHeaders(request);
         printHeaderUtils(request);
         printEtc(request);
-
+        response.getWriter().write("ok");
     }
 
-    private static void printStartLine(HttpServletRequest request) {
-        //start line 정보
+    //start line 정보
+    private void printStartLine(HttpServletRequest request) {
         System.out.println("--- REQUEST-LINE - start ---");
         System.out.println("request.getMethod() = " + request.getMethod()); //GET
-        System.out.println("request.getProtocol() = " + request.getProtocol());
-        //HTTP/1.1
+        System.out.println("request.getProtocol() = " + request.getProtocol()); //HTTP/1.1
         System.out.println("request.getScheme() = " + request.getScheme()); //http
         // http://localhost:8080/request-header
         System.out.println("request.getRequestURL() = " + request.getRequestURL());
-        // /request-header
+        ///request-header
         System.out.println("request.getRequestURI() = " + request.getRequestURI());
         //username=hi
         System.out.println("request.getQueryString() = " +
@@ -44,23 +40,15 @@ public class RequestHeaderServlet extends HttpServlet {
 
     //Header 모든 정보
     private void printHeaders(HttpServletRequest request) {
-
         System.out.println("--- Headers - start ---");
-
-        /* Enumeration<String> headerNames = request.getHeaderNames();
-        while(headerNames.hasMoreElements()) {
-            String headerName = headerNames.nextElement();
-            System.out.println(headerName + " : " + headerName);
-        } ↓ 아래 문장으로 치환 가능 */
-
-        request.getHeaderNames().asIterator().forEachRemaining(headerName -> System.out.println(headerName + " : " + headerName));
 
         request.getHeaderNames().asIterator()
                 .forEachRemaining(headerName -> System.out.println(headerName + ": " + request.getHeader(headerName)));
-
-                        System.out.println("--- Headers - end ---");
+        System.out.println("--- Headers - end ---");
         System.out.println();
     }
+
+//--- Headers - end ---
 
     //Header 편리한 조회
     private void printHeaderUtils(HttpServletRequest request) {
@@ -68,7 +56,6 @@ public class RequestHeaderServlet extends HttpServlet {
         System.out.println("[Host 편의 조회]");
         System.out.println("request.getServerName() = " +
                 request.getServerName()); //Host 헤더
-
         System.out.println("request.getServerPort() = " +
                 request.getServerPort()); //Host 헤더
         System.out.println();
@@ -115,7 +102,5 @@ public class RequestHeaderServlet extends HttpServlet {
         System.out.println("request.getLocalPort() = " +
                 request.getLocalPort()); //
         System.out.println("--- 기타 조회 end ---");
-        System.out.println();
     }
-
 }
