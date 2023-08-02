b36a4938eb5bf1b5e0c64dfad294d353a73755d9
minji6313 <minji6313@yahoo.com>
2023-08-02 16:33:21 +0900

    "#JPA-6 v3 Entity를 DTO로 변환"

--- jpashop/src/main/java/com/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/com/jpabook/jpashop/api/OrderApiController.java
@@ -10,6 +10,7 @@ import com.jpabook.jpashop.repository.OrderSearch;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -52,13 +53,14 @@ public class OrderApiController {
             /** 3. : 조회된 주문 엔티티들을 OrderDto로 변환하는 과정입니다. orders.stream()을 사용하여 주문 엔티티들을 스트림으로 변환하고,
              * .map(o -> new OrderDto(o))를 사용하여 각 주문 엔티티를 OrderDto로 변환합니다. 이때 람다 표현식을 사용하여 각 주문 엔티티를 OrderDto로 매핑합니다.
              * 변환된 OrderDto들은 .collect(Collectors.toList())를 사용하여 List로 수집합니다. */
-            List<OrderDto> collect = orders.stream()
+            List<OrderDto> result = orders.stream()
                     .map(o -> new OrderDto(o))
                     .collect(Collectors.toList());
 
 
-            return collect;
+            return result;
         }
+
 
         /** 4.static class OrderDto { ... }: OrderDto는 Order 엔티티의 정보를 가지고 필요한 정보만을 선택하여 DTO(Data Transfer Object)로 표현하는 클래스입니다.
          * DTO는 엔티티의 정보를 외부로 전달할 때 사용되며, API 응답 등에서 엔티티의 데이터를 노출하지 않고 필요한 정보만 전달하는 데 유용합니다. */
@@ -70,20 +72,53 @@ public class OrderApiController {
             private LocalDateTime orderDate;
             private OrderStatus orderStatus;
             private Address address;
-            private List<OrderItem> orderItems;
+            private List<OrderItemDto> orderItems;
 
             /** 5.public OrderDto(Order o) { ... }: OrderDto 클래스의 생성자는 Order 엔티티를 인자로 받아서 OrderDto 객체로 변환하는 역할을 합니다.
              * 따라서 이 생성자에서 Order 엔티티의 정보를 활용하여 OrderDto의 필드들을 초기화하는 로직을 작성해야 합니다.
-             * 현재 코드에서는 생성자 내용이 비어 있으므로 Order 엔티티의 정보를 활용하여 OrderDto를 적절하게 구성하는 로직을 추가해야 합니다. */
+             * 현재 코드에서는 생성자 내용이 작성되어 있으며, Order 엔티티의 필드들을 활용하여 OrderDto를 적절하게 구성하는 로직을 추가했습니다. */
             public OrderDto(Order order) {
                 orderId = order.getId();
                 name = order.getMember().getName();
                 orderDate = order.getOrderDate();
                 orderStatus = order.getStatus();
                 address = order.getDelivery().getAddress();
-                order.getOrderItems().stream().forEach(o-> o.getItem().getName());
-                orderItems = order.getOrderItems(); //orderItems는 현재 entity이기 때문에 null이 출력
+                orderItems = order.getOrderItems().stream()
+                        .map(orderItem -> new OrderItemDto(orderItem))
+                        .collect(Collectors.toList());
+                //order.getOrderItems().stream().forEach(o-> o.getItem().getName());
+                //orderItems = order.getOrderItems(); //orderItems는 현재 entity이기 때문에 null이 출력
             }
         }
 
+        @Getter
+        static class  OrderItemDto{
+
+            private String itemName; //상품명
+            private int orderPrice;  //상품 가격
+            private int count;       //주문 수량
+
+            public OrderItemDto(OrderItem orderItem) {
+
+                itemName = orderItem.getItem().getName();
+                orderPrice = orderItem.getOrderPrice();
+                count = orderItem.getCount();
+            }
+        }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
